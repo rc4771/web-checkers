f24a18c455c3a806da21b53a02f524d4c770f46a
DavidTheFighter <19dallen@gmail.com>
2020-09-23 15:25:58 -0400

    Added barebones sign in route handler and FTL file

---
+++ src/main/java/com/webcheckers/ui/GetSignInRoute.java
@@ -0,0 +1,54 @@
+package com.webcheckers.ui;
+
+import com.webcheckers.util.Message;
+import spark.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.logging.Logger;
+
+/**
+ * The UI Controller to GET the Home page.
+ *
+ * @author <a href='mailto:bdbvse@rit.edu'>Bryan Basham</a>
+ */
+public class GetSignInRoute implements Route {
+  private static final Logger LOG = Logger.getLogger(GetSignInRoute.class.getName());
+
+  private final TemplateEngine templateEngine;
+
+  /**
+   * Create the Spark Route (UI controller) to handle all {@code GET /} HTTP requests.
+   *
+   * @param templateEngine
+   *   the HTML template rendering engine
+   */
+  public GetSignInRoute(final TemplateEngine templateEngine) {
+    this.templateEngine = Objects.requireNonNull(templateEngine, "templateEngine is required");
+    //
+    LOG.config("GetHomeRoute is initialized.");
+  }
+
+  /**
+   * Render the WebCheckers Home page.
+   *
+   * @param request
+   *   the HTTP request
+   * @param response
+   *   the HTTP response
+   *
+   * @return
+   *   the rendered HTML for the Home page
+   */
+  @Override
+  public Object handle(Request request, Response response) {
+    LOG.finer("GetSignInRoute is invoked.");
+    //
+    Map<String, Object> vm = new HashMap<>();
+    vm.put("title", "Sign in?");
+
+    // render the View
+    return templateEngine.render(new ModelAndView(vm , "signin.ftl"));
+  }
+}
---
+++ src/main/resources/spark/template/freemarker/signin.ftl
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></meta>
+  <meta http-equiv="refresh" content="10">
+  <title>Web Checkers | ${title}</title>
+  <link rel="stylesheet" type="text/css" href="/css/style.css">
+</head>
+
+<body>
+<div class="page">
+
+  <h1>Web Checkers | ${title}</h1>
+
+</div>
+</body>
+
+</html>
