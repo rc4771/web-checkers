9a5932f78bb6a0d31940ddd57316fb7683853a26
Subzero6671 <wsp6945@rit.edu>
2020-10-27 08:02:48 -0400

    Moved unit tests

--- src/test/java/com/webcheckers/model/SpaceTest.java
+++ src/test/java/com/webcheckers/model/SpaceTest.java
@@ -19,39 +19,4 @@ public class SpaceTest {
         s = new BlackSpace(42, null);
         assertEquals(s.getCellIdx(), 42);
     }
-
-    @Test
-    public void testWhiteSpace_isValid() {
-        assertFalse(new WhiteSpace(0).isValid());
-    }
-
-    @Test
-    public void testBlackSpace_isValid_nullPiece() {
-        assertTrue(new BlackSpace(0, null).isValid());
-    }
-
-    @Test
-    public void testBlackSpace_isValid() {
-        assertFalse(new BlackSpace(0, new RedSinglePiece()).isValid());
-    }
-
-    @Test
-    public void testBlackSpace_getPiece_nullPiece(){
-        assertNull(new BlackSpace(0).getPiece());
-    }
-
-    @Test
-    public void testBlackSpace_getPiece() {
-        Piece p = new RedSinglePiece();
-        assertEquals(p, new BlackSpace(0, p).getPiece());
-    }
-
-    @Test
-    public void testBlackSpace_setPiece() {
-        BlackSpace s = new BlackSpace(0);
-        Piece p = new RedSinglePiece();
-        s.setPiece(p);
-        
-        assertEquals(p, s.getPiece());
-    }
 }
