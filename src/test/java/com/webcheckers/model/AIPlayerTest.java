1bf4317747e933e0f272d5e364e64f9dc1c32a48
Subzero6671 <wsp6945@rit.edu>
2020-11-04 14:10:33 -0500

    created aiplayertest class

---
+++ src/test/java/com/webcheckers/model/AIPlayerTest.java
@@ -0,0 +1,12 @@
+package com.webcheckers.model;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import static org.junit.jupiter.api.Assertions.*;
+import org.mockito.Mockito.*;
+
+@Tag("Model-tier")
+public class AIPlayerTest {
+
+}
