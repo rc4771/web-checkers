86c48eefaf85e02b6977bc6181ac898b5e8529c8
Subzero6671 <wpetersen@rocketmail.com>
2020-11-04 19:46:17 -0500

    aiplayertest

--- src/test/java/com/webcheckers/model/AIPlayerTest.java
+++ src/test/java/com/webcheckers/model/AIPlayerTest.java
@@ -3,10 +3,30 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.mockito.stubbing.Answer;
 import static org.junit.jupiter.api.Assertions.*;
-import org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
 
 @Tag("Model-tier")
 public class AIPlayerTest {
+
+    @Test
+    void testConstructor(){
+    AIPlayer CuT = new AIPlayer(3);
+    assertEquals(CuT.getName(), CuT.ROBOT_EMOJI + CuT.AI_NAMES[3]);
+    }
+
+    @Test
+    void testGetAIPlayerIndex(){
+    AIPlayer CuT = new AIPlayer(1);
+    assertEquals(CuT.getAIPlayerIndex(), 1);
+    }
+    
+
+
+
 
 }
